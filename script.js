d63565e94922f616487ab36e8fc9fcb5aa432424
asdnikita <lv26690451@gmail.com>
2020-08-13 16:04:02 +0300

    animation background

--- script.js
+++ script.js
@@ -6,9 +6,9 @@
         if (difference > 0) {
           const parts = {
             days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-            minutes: Math.floor((difference / 1000 / 60) % 60),
-            seconds: Math.floor((difference / 1000) % 60)
+            h: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            min: Math.floor((difference / 1000 / 60) % 60),
+            sec: Math.floor((difference / 1000) % 60)
           };
 
           remaining = Object.keys(parts)
